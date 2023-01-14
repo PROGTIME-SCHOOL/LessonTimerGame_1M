04ce549e327c5499780837ccaed1955609fe1f6b
PROGTIME-SCHOOL <117563363+PROGTIME-SCHOOL@users.noreply.github.com>
2023-01-14 11:30:21 +0300

    Initial commit

---
+++ FlyGame/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace FlyGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonFly = new System.Windows.Forms.Button();
+            this.timerGame = new System.Windows.Forms.Timer(this.components);
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonFly
+            // 
+            this.buttonFly.Location = new System.Drawing.Point(191, 185);
+            this.buttonFly.Name = "buttonFly";
+            this.buttonFly.Size = new System.Drawing.Size(80, 63);
+            this.buttonFly.TabIndex = 0;
+            this.buttonFly.Text = "FLY";
+            this.buttonFly.UseVisualStyleBackColor = true;
+            this.buttonFly.Click += new System.EventHandler(this.buttonFly_Click);
+            // 
+            // timerGame
+            // 
+            this.timerGame.Interval = 1000;
+            this.timerGame.Tick += new System.EventHandler(this.timerGame_Tick);
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Location = new System.Drawing.Point(12, 9);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(92, 25);
+            this.labelScore.TabIndex = 1;
+            this.labelScore.Text = "Score: 0";
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Location = new System.Drawing.Point(12, 34);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(102, 25);
+            this.labelTimer.TabIndex = 2;
+            this.labelTimer.Text = "Timer: 10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 429);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.buttonFly);
+            this.Name = "Form1";
+            this.Text = "Fly Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFly;
+        private System.Windows.Forms.Timer timerGame;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelTimer;
+    }
+}
+
---
+++ FlyGame/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlyGame
+{
+    public partial class Form1 : Form
+    {
+        // Глобальные переменные
+        int score = 0;
+        int timer = 10;
+        bool isRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Width = 500;
+            this.Height = 500;
+        }
+
+        private void buttonFly_Click(object sender, EventArgs e)
+        {
+            if (isRunning == false)
+            {
+                timerGame.Enabled = true;
+                isRunning = true;
+            }
+            
+
+            #region Move Fly
+
+            // RANDOM
+            Random r = new Random();  // cоздание инструмента рандом
+
+            int x = r.Next(0, 500 - buttonFly.Width - 20);
+            int y = r.Next(0, 500 - buttonFly.Height - 20);
+
+            buttonFly.Left = x;
+            buttonFly.Top = y;
+
+            #endregion
+
+            score++;
+            labelScore.Text = "Score: " + score.ToString();
+
+            //score = score + 1;
+            //score += 1;
+        }
+
+        private void timerGame_Tick(object sender, EventArgs e)
+        {
+            timer--;  // уменьшаем время на 1
+            labelTimer.Text = "Timer: " + timer.ToString();
+
+            if (timer == 0)
+            {
+                timerGame.Enabled = false;   // останавливаем таймер
+                buttonFly.Enabled = false;   // отключаем нажатие на кнопку
+
+                // итоги игры
+                if (score < 10)
+                {
+                    MessageBox.Show("Bad");
+                }
+                else if (score < 14)
+                {
+                    MessageBox.Show("Normal");
+                }
+                else
+                {
+                    MessageBox.Show("Cool!");
+                }
+
+            }
+        }
+    }
+}
