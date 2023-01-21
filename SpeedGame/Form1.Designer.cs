24be22aa2f9e75c6d949e9460fcf5f699bf24291
PROGTIME-SCHOOL <117563363+PROGTIME-SCHOOL@users.noreply.github.com>
2023-01-21 11:30:59 +0300

    SpeedGame_Part2

--- SpeedGame/Form1.Designer.cs
+++ SpeedGame/Form1.Designer.cs
@@ -28,59 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxCar1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxCar2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxCar3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxCar4 = new System.Windows.Forms.PictureBox();
+            this.timerGame = new System.Windows.Forms.Timer(this.components);
+            this.buttonFinish = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCar4)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pictureBoxCar1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = global::SpeedGame.Properties.Resources.car2;
-            this.pictureBox1.Location = new System.Drawing.Point(32, 100);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(146, 68);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pictureBoxCar1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxCar1.Image = global::SpeedGame.Properties.Resources.car2;
+            this.pictureBoxCar1.Location = new System.Drawing.Point(32, 100);
+            this.pictureBoxCar1.Name = "pictureBoxCar1";
+            this.pictureBoxCar1.Size = new System.Drawing.Size(146, 68);
+            this.pictureBoxCar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxCar1.TabIndex = 0;
+            this.pictureBoxCar1.TabStop = false;
             // 
-            // pictureBox2
+            // pictureBoxCar2
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.Image = global::SpeedGame.Properties.Resources.car2_pink;
-            this.pictureBox2.Location = new System.Drawing.Point(32, 229);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(146, 68);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.pictureBoxCar2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxCar2.Image = global::SpeedGame.Properties.Resources.car2_pink;
+            this.pictureBoxCar2.Location = new System.Drawing.Point(32, 229);
+            this.pictureBoxCar2.Name = "pictureBoxCar2";
+            this.pictureBoxCar2.Size = new System.Drawing.Size(146, 68);
+            this.pictureBoxCar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxCar2.TabIndex = 1;
+            this.pictureBoxCar2.TabStop = false;
             // 
-            // pictureBox3
+            // pictureBoxCar3
             // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox3.Image = global::SpeedGame.Properties.Resources.car2_green;
-            this.pictureBox3.Location = new System.Drawing.Point(32, 357);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(146, 68);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            this.pictureBoxCar3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxCar3.Image = global::SpeedGame.Properties.Resources.car2_green;
+            this.pictureBoxCar3.Location = new System.Drawing.Point(32, 357);
+            this.pictureBoxCar3.Name = "pictureBoxCar3";
+            this.pictureBoxCar3.Size = new System.Drawing.Size(146, 68);
+            this.pictureBoxCar3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxCar3.TabIndex = 2;
+            this.pictureBoxCar3.TabStop = false;
             // 
-            // pictureBox4
+            // pictureBoxCar4
             // 
-            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox4.Image = global::SpeedGame.Properties.Resources.car2_yellow;
-            this.pictureBox4.Location = new System.Drawing.Point(32, 487);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(146, 68);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox4.TabIndex = 3;
-            this.pictureBox4.TabStop = false;
+            this.pictureBoxCar4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxCar4.Image = global::SpeedGame.Properties.Resources.car2_yellow;
+            this.pictureBoxCar4.Location = new System.Drawing.Point(32, 487);
+            this.pictureBoxCar4.Name = "pictureBoxCar4";
+            this.pictureBoxCar4.Size = new System.Drawing.Size(146, 68);
+            this.pictureBoxCar4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxCar4.TabIndex = 3;
+            this.pictureBoxCar4.TabStop = false;
+            // 
+            // timerGame
+            // 
+            this.timerGame.Tick += new System.EventHandler(this.timerGame_Tick);
+            // 
+            // buttonFinish
+            // 
+            this.buttonFinish.Enabled = false;
+            this.buttonFinish.Location = new System.Drawing.Point(1018, 100);
+            this.buttonFinish.Name = "buttonFinish";
+            this.buttonFinish.Size = new System.Drawing.Size(10, 439);
+            this.buttonFinish.TabIndex = 4;
+            this.buttonFinish.UseVisualStyleBackColor = true;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(4, 583);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(192, 60);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(202, 583);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 33);
+            this.comboBox1.TabIndex = 6;
             // 
             // Form1
             // 
@@ -89,27 +125,34 @@
             this.BackgroundImage = global::SpeedGame.Properties.Resources.road_bg;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(1127, 653);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBoxCar4);
+            this.Controls.Add(this.pictureBoxCar3);
+            this.Controls.Add(this.pictureBoxCar2);
+            this.Controls.Add(this.pictureBoxCar1);
+            this.Controls.Add(this.buttonFinish);
             this.DoubleBuffered = true;
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.Text = "Speed Game v1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCar4)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBoxCar1;
+        private System.Windows.Forms.PictureBox pictureBoxCar2;
+        private System.Windows.Forms.PictureBox pictureBoxCar3;
+        private System.Windows.Forms.PictureBox pictureBoxCar4;
+        private System.Windows.Forms.Timer timerGame;
+        private System.Windows.Forms.Button buttonFinish;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- SpeedGame/Form1.cs
+++ SpeedGame/Form1.cs
@@ -12,9 +12,59 @@ namespace SpeedGame
 {
     public partial class Form1 : Form
     {
+        // global variables
+        Random r = new Random();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timerGame_Tick(object sender, EventArgs e)
+        {
+            //r.Next(0, 10);
+
+            // подключить двойной буфер 
+
+            pictureBoxCar1.Left = pictureBoxCar1.Left + r.Next(0, 10);
+            pictureBoxCar2.Left = pictureBoxCar2.Left + r.Next(0, 10);
+            pictureBoxCar3.Left = pictureBoxCar3.Left + r.Next(0, 10);
+            pictureBoxCar4.Left = pictureBoxCar4.Left + r.Next(0, 10);
+
+            /*if (pictureBoxCar1.Right >= buttonFinish.Right ||
+                pictureBoxCar2.Right >= buttonFinish.Right ||
+                pictureBoxCar3.Right >= buttonFinish.Right ||
+                pictureBoxCar4.Right >= buttonFinish.Right)
+            {
+                timerGame.Enabled = false;
+            } */
+
+
+            if (pictureBoxCar1.Right >= buttonFinish.Right)
+            {
+                timerGame.Enabled = false;
+                MessageBox.Show("Red car has won!");
+            }
+            else if (pictureBoxCar2.Right >= buttonFinish.Right)
+            {
+                timerGame.Enabled = false;
+                MessageBox.Show("Pink car has won!");
+            }
+            else if (pictureBoxCar3.Right >= buttonFinish.Right)
+            {
+                timerGame.Enabled = false;
+                MessageBox.Show("Green car has won!");
+            }
+            else if (pictureBoxCar4.Right >= buttonFinish.Right)
+            {
+                timerGame.Enabled = false;
+                MessageBox.Show("Yellow car has won!");
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            timerGame.Enabled = true;
         }
     }
 }
